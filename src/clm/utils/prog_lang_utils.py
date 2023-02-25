120dde741cbfadbd917cebe0bc89d9e232671f80
Matthias Hölzl <tc@xantira.com>
2023-02-25 18:48:05 +0100

    Fix bug that prevents rust code files from being generated

--- src/clm/utils/prog_lang_utils.py
+++ src/clm/utils/prog_lang_utils.py
@@ -2,7 +2,7 @@ def suffix_for(prog_lang: str) -> str:
     if prog_lang == "python":
         return "py"
     elif prog_lang == "rust":
-        return "ru"
+        return "rs"
     else:
         raise ValueError(f"Unsupported language: {prog_lang}")
 
