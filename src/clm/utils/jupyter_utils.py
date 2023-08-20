2188157825c3266e3209af0b774a2b275970afff
Matthias Hölzl <tc@xantira.com>
2023-08-20 23:18:24 +0200

    Format with black
    Also make `cell_from_string()` test function public.

--- src/clm/utils/jupyter_utils.py
+++ src/clm/utils/jupyter_utils.py
@@ -8,7 +8,7 @@ from nbformat import NotebookNode
 from clm.utils.path_utils import sanitize_file_name
 
 # %%
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
 # %%
@@ -18,31 +18,31 @@ Cell: TypeAlias = NotebookNode
 # %%
 def get_cell_type(cell: Cell) -> str:
     """Return the type of `cell`."""
-    return cell['cell_type']
+    return cell["cell_type"]
 
 
 def is_code_cell(cell):
     """Returns whether a cell is a code cell."""
-    return get_cell_type(cell) == 'code'
+    return get_cell_type(cell) == "code"
 
 
 def is_markdown_cell(cell):
     """Returns whether a cell is a code cell."""
-    return get_cell_type(cell) == 'markdown'
+    return get_cell_type(cell) == "markdown"
 
 
 # %%
 def get_tags(cell: Cell) -> list[str]:
     """Return the tags for `cell`."""
-    return cell['metadata'].get('tags', [])
+    return cell["metadata"].get("tags", [])
 
 
-def set_tags(cell: Cell, tags: list['str']) -> None:
+def set_tags(cell: Cell, tags: list["str"]) -> None:
     """Set the tags for `cell`."""
     if tags:
-        cell['metadata']['tags'] = tags
-    elif cell['metadata'].get('tags') is not None:
-        del cell['metadata']['tags']
+        cell["metadata"]["tags"] = tags
+    elif cell["metadata"].get("tags") is not None:
+        del cell["metadata"]["tags"]
 
 
 # %%
@@ -59,26 +59,26 @@ def get_cell_language(cell) -> str:
     An empty language code means the cell has no specified language and should therefore
     be included in all language outputs."""
 
-    return cell['metadata'].get('lang', '')
+    return cell["metadata"].get("lang", "")
 
 
 # %%
 # Tags that control the behavior of this cell in a slideshow
-_SLIDE_TAGS = {'slide', 'subslide', 'notes'}
+_SLIDE_TAGS = {"slide", "subslide", "notes"}
 # Tags that prevent this cell from being publicly visible
-_PRIVATE_TAGS = {'notes', 'private'}
+_PRIVATE_TAGS = {"notes", "private"}
 # Tags that may appear in any kind of cell
-_EXPECTED_GENERIC_TAGS = _SLIDE_TAGS | _PRIVATE_TAGS | {'alt', 'del'}
+_EXPECTED_GENERIC_TAGS = _SLIDE_TAGS | _PRIVATE_TAGS | {"alt", "del"}
 # Tags that may appear in code cells (in addition to generic tags)
-_EXPECTED_CODE_TAGS = {'keep', 'start'} | _EXPECTED_GENERIC_TAGS
+_EXPECTED_CODE_TAGS = {"keep", "start"} | _EXPECTED_GENERIC_TAGS
 # Tags that may appear in markdown cells (in addition to generic tags)
-_EXPECTED_MARKDOWN_TAGS = {'notes', 'answer'} | _EXPECTED_GENERIC_TAGS
+_EXPECTED_MARKDOWN_TAGS = {"notes", "answer"} | _EXPECTED_GENERIC_TAGS
 
 
 # %%
 def is_deleted_cell(cell: Cell):
     """Return whether a cell has been deleted."""
-    return 'del' in get_tags(cell)
+    return "del" in get_tags(cell)
 
 
 # %%
@@ -99,7 +99,7 @@ def is_starting_cell(cell: Cell):
 
     Starting points should be completely removed from completed notebooks, but they
     cells should be included in codealongs."""
-    return 'start' in get_tags(cell)
+    return "start" in get_tags(cell)
 
 
 # %%
@@ -108,7 +108,7 @@ def is_alternate_solution(cell: Cell):
 
     Alternate solutions should be present in completed notebooks, but their cells should
     be completely removed from codealongs (not included as empty cells)."""
-    return 'alt' in get_tags(cell)
+    return "alt" in get_tags(cell)
 
 
 # %%
@@ -122,9 +122,9 @@ def is_answer_cell(cell: Cell):
     - Markdown cells are only answers if they have the `answer` tag."""
 
     if is_code_cell(cell):
-        return not {'keep', 'start'}.intersection(get_tags(cell))
+        return not {"keep", "start"}.intersection(get_tags(cell))
     else:
-        return 'answer' in get_tags(cell)
+        return "answer" in get_tags(cell)
 
 
 # %%
@@ -138,7 +138,7 @@ def get_slide_tag(cell: Cell) -> str | None:
     if slide_tags:
         if len(slide_tags) > 1:
             logging.warning(
-                f'Found more than one slide tag: {slide_tags}. Picking one at random.'
+                f"Found more than one slide tag: {slide_tags}. Picking one at random."
             )
         return slide_tags.pop()
     else:
@@ -160,14 +160,14 @@ def is_cell_included_for_language(cell: Cell, lang: str) -> bool:
 def warn_on_invalid_code_tags(tags):
     for tag in tags:
         if tag not in _EXPECTED_CODE_TAGS:
-            logging.warning(f'Unknown tag for code cell: {tag!r}.')
+            logging.warning(f"Unknown tag for code cell: {tag!r}.")
 
 
 # %%
 def warn_on_invalid_markdown_tags(tags):
     for tag in tags:
         if tag not in _EXPECTED_MARKDOWN_TAGS:
-            logging.warning(f'Unknown tag for markdown cell: {tag!r}.')
+            logging.warning(f"Unknown tag for markdown cell: {tag!r}.")
 
 
 # %%
@@ -177,15 +177,13 @@ TITLE_REGEX = re.compile(
 
 
 # %%
-def find_notebook_titles(
-    text: str, default: str = 'unnamed'
-) -> dict[str, str]:
+def find_notebook_titles(text: str, default: str = "unnamed") -> dict[str, str]:
     """Find the titles from the source text of a notebook."""
     match = TITLE_REGEX.search(text)
     if match:
         return {
-            'en': sanitize_file_name(match[2]),
-            'de': sanitize_file_name(match[1]),
+            "en": sanitize_file_name(match[2]),
+            "de": sanitize_file_name(match[1]),
         }
     else:
-        return {'en': default, 'de': default}
+        return {"en": default, "de": default}
--- tests/cell_fixtures.py
+++ tests/cell_fixtures.py
@@ -7,97 +7,95 @@ from nbformat import NotebookNode
 Cell: TypeAlias = NotebookNode
 
 
-def _cell_from_string(header_line: str, *body_lines: str) -> Cell:
+def cell_from_string(header_line: str, *body_lines: str) -> Cell:
     return jupytext.reads(
-        '\n'.join([header_line, *body_lines]), format='py:percent'
+        "\n".join([header_line, *body_lines]), format="py:percent"
     ).cells[0]
 
 
 @pytest.fixture
 def code_cell() -> Cell:
-    return _cell_from_string('# %%', 'default_int = 1 + 1')
+    return cell_from_string("# %%", "default_int = 1 + 1")
 
 
 @pytest.fixture
 def english_code_cell() -> Cell:
-    return _cell_from_string('# %% lang="en"', 'english_int = 1 + 1')
+    return cell_from_string('# %% lang="en"', "english_int = 1 + 1")
 
 
 @pytest.fixture
 def german_code_cell() -> Cell:
-    return _cell_from_string('# %% lang="de"', 'german_int = 1 + 1')
+    return cell_from_string('# %% lang="de"', "german_int = 1 + 1")
 
 
 @pytest.fixture
 def code_slide_cell() -> Cell:
-    return _cell_from_string('# %% tags=["slide"]', 'slide_int = 123')
+    return cell_from_string('# %% tags=["slide"]', "slide_int = 123")
 
 
 @pytest.fixture
 def code_subslide_cell() -> Cell:
-    return _cell_from_string('# %% tags=["subslide"]', 'subslide_int = 234')
+    return cell_from_string('# %% tags=["subslide"]', "subslide_int = 234")
 
 
 @pytest.fixture
 def kept_cell() -> Cell:
-    return _cell_from_string('# %% tags=["keep"]', 'kept_int = 2 + 2')
+    return cell_from_string('# %% tags=["keep"]', "kept_int = 2 + 2")
 
 
 @pytest.fixture
 def alternate_cell() -> Cell:
-    return _cell_from_string('# %% tags=["alt"]', 'alternate_int = 3 + 3')
+    return cell_from_string('# %% tags=["alt"]', "alternate_int = 3 + 3")
 
 
 @pytest.fixture
 def deleted_cell() -> Cell:
-    return _cell_from_string('# %% tags=["del"]', 'deleted_int = 3 + 3')
+    return cell_from_string('# %% tags=["del"]', "deleted_int = 3 + 3")
 
 
 @pytest.fixture
 def starting_cell() -> Cell:
-    return _cell_from_string('# %% tags=["start"]', 'start_val = 123')
+    return cell_from_string('# %% tags=["start"]', "start_val = 123")
 
 
 @pytest.fixture
 def markdown_cell() -> Cell:
-    return _cell_from_string('# %% [markdown]', 'Some text')
+    return cell_from_string("# %% [markdown]", "Some text")
 
 
 @pytest.fixture
 def english_markdown_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] lang="en"', 'Text in English.')
+    return cell_from_string('# %% [markdown] lang="en"', "Text in English.")
 
 
 @pytest.fixture
 def german_markdown_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] lang="de"', 'Text in Deutsch.')
+    return cell_from_string('# %% [markdown] lang="de"', "Text in Deutsch.")
 
 
 @pytest.fixture
 def markdown_slide_cell() -> Cell:
-    return _cell_from_string(
-        '# %% [markdown] tags=["slide"]', 'Some more text'
-    )
+    return cell_from_string('# %% [markdown] tags=["slide"]', "Some more text")
 
 
 @pytest.fixture
 def markdown_subslide_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] tags=["subslide"]', 'A note.')
+    return cell_from_string('# %% [markdown] tags=["subslide"]', "A note.")
 
 
 @pytest.fixture
 def deleted_markdown_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] tags=["del"]', 'A deleted cell.')
+    return cell_from_string('# %% [markdown] tags=["del"]', "A deleted cell.")
 
 
 @pytest.fixture
 def markdown_notes_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] tags=["notes"]', 'A note.')
+    return cell_from_string('# %% [markdown] tags=["notes"]', "A note.")
 
 
 @pytest.fixture
 def answer_cell() -> Cell:
-    return _cell_from_string('# %% [markdown] tags=["answer"]', 'An answer.')
+    return cell_from_string('# %% [markdown] tags=["answer"]', "An answer.")
 
 
 @pytest.fixture
