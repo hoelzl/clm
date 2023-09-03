d01b31a017258e8a78e41dd4cc34b02946731bbd
Matthias Hölzl <tc@xantira.com>
2023-09-03 13:03:42 +0200

    Add some more tests for course layouts

--- tests/specs_tests/course_layouts_test.py
+++ tests/specs_tests/course_layouts_test.py
@@ -1,7 +1,40 @@
 from pathlib import Path
 
-from clm.specs.course_layouts import legacy_python_course_layout
-from clm.specs.directory_kinds import NotebookDirectory
+from configurator import Config
+
+from clm.core.course_layout import course_layout_registry
+from clm.core.directory_kind import GeneralDirectory
+from clm.specs.course_layouts import (
+    legacy_python_course_layout,
+    create_layouts_from_config,
+)
+from clm.specs.directory_kinds import NotebookDirectory, LegacyExampleDirectory
+
+_course_layouts = [
+    {
+        "name": "test-layout",
+        "default_directory_kind": "GeneralDirectory",
+        "directory_patterns": [
+            ("code/completed", "LegacyExampleDirectory"),
+            ("slides/*", "NotebookDirectory"),
+            ("slides/*/img/**", "GeneralDirectory"),
+        ],
+    },
+]
+
+
+def test_create_layouts_from_config():
+    config = Config({"course_layouts": _course_layouts})
+    create_layouts_from_config(config)
+    assert course_layout_registry.get("test-layout") is not None
+    layout = course_layout_registry["test-layout"]
+    assert layout.name == "test-layout"
+    assert layout.default_directory_kind == GeneralDirectory()
+    assert layout.directory_patterns == (
+        ("code/completed", LegacyExampleDirectory),
+        ("slides/*", NotebookDirectory),
+        ("slides/*/img/**", GeneralDirectory),
+    )
 
 
 def test_legacy_python_course_layout():
